a775cd38dc0baecec34c5d4cee6d9d0c21127512
Manuel <m24.tapia@gmail.com>
2021-03-14 01:23:01 -0300

    ref: deleting mapper service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,9 +1,7 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { getConnection } from 'typeorm';
-import { MapperService } from '../../shared/mapper.service';
 import { Role } from '../role/role.entity';
-import { UserDto } from './dto/user.dto';
 import { UserDetails } from './user.details.entity';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
@@ -13,12 +11,11 @@ export class UserService {
 
   constructor(
     @InjectRepository(UserRepository)
-    private readonly _userRepository: UserRepository,
-    private readonly _mapperService: MapperService
+    private readonly _userRepository: UserRepository
   ){
   }
   
-  async get(id:number):Promise<UserDto>{
+  async get(id:number):Promise<User>{
     if(!id)
       throw new BadRequestException('id must be sent');
 
@@ -29,19 +26,19 @@ export class UserService {
     if(!user)
       throw new NotFoundException();
 
-    return this._mapperService.map<User,UserDto>(user, new UserDto());
+    return user;
   }
 
-  async getAll():Promise<UserDto[]>{
+  async getAll():Promise<User[]>{
 
     const users:User[] = await this._userRepository.find({
       where:{status:'ACTIVE'}
     });
 
-    return this._mapperService.mapCollection<User,UserDto>(users, new UserDto());
+    return users;
   }
 
-  async create(user:User):Promise<UserDto>{
+  async create(user:User):Promise<User>{
     const details = new UserDetails();
     user.details = details;
 
@@ -49,7 +46,7 @@ export class UserService {
     const defaultRole = await repo.findOne({where:{name:'GENERAL'}});
     user.roles = [defaultRole];
     const savedUser:User = await this._userRepository.save(user);
-    return this._mapperService.map<User,UserDto>(savedUser,new UserDto());
+    return savedUser;
   }
 
   async update(id:number,user:User):Promise<void>{
